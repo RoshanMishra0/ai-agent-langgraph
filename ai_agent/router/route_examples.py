4faa5365c772bd12ffab6aa20c873dce8d06cdb5
RoshanMishra0 <roshanmishra2003@gmail.com>
2026-05-07 19:41:29 +0530

    feat: Examples to detect routes

---
+++ ai_agent/router/route_examples.py
@@ -0,0 +1,16 @@
+route_examples = {
+    "math": [
+        "calculate numbers",
+        "solve math problem",
+        "what is 5 plus 5"
+    ],
+    "weather": [
+        "weather today",
+        "temperature in city",
+        "is it raining"
+    ],
+    "general": [
+        "tell me a joke",
+        "write a poem"
+    ]
+}
